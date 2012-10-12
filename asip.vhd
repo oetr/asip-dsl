2a560472e83112392b668b1db04b6407695e3ba0
Petr Samarin <petrsamarin@gmail.com>
2012-10-12 13:59:41 +0200

    Turn on LEDs and wait for a while.

--- asip.vhd
+++ asip.vhd
@@ -5,26 +5,27 @@ use work.instructions_lib.all;
 ----------------------------------------------------------------------
 entity asip is
   port (
-    clk   : in  std_logic;
-    reset : in  std_logic;
-    oLEDR : out std_logic_vector (17 downto 0);
+    iCLK_50 : in  std_logic;
+    iKEY    : in  std_logic_vector(3 downto 0);
+    oLEDR   : out std_logic_vector (17 downto 0)
     );
 end entity asip;
 ----------------------------------------------------------------------
 architecture arch of asip is
-  --------------------------------------------------------------------
-  -- ASIP
-  --------------------------------------------------------------------
-  -- constants
-  constant INSTR_WIDTH : integer := 4;
-  -- prepare: 16 32-bit registers
-  constant REG_WIDTH   : integer := 32;
-  constant REG_NUM     : integer := 16;
-  type reg_type is array (natural range 0 to REG_NUM-1) of
-    std_logic_vector (REG_WIDTH-1 downto 0);
+  -- IO signals
+  signal clk, reset     : std_logic;
   -- declare: 16 32-bit registers
   signal registers_reg  : reg_type;
   signal registers_next : reg_type;
+
+  -- instruction counter
+  signal i_reg, i_next : integer range 0 to INSTRUCTION_COUNT-1;
+
+  -- wait counter
+  signal wait_counter_reg  : integer range 0 to INSTRUCTION_COUNT + REG_NUM_BITS - 1;
+  signal wait_counter_next : integer range 0 to INSTRUCTION_COUNT + REG_NUM_BITS - 1;
+
+  signal started_reg, started_next : boolean;
   
 begin
   --------------------------------------------------------------------
@@ -33,19 +34,75 @@ begin
   process (clk, reset) is
   begin
     if reset = '1' then
-      registers_reg <= (others => (others => '0'));
+      registers_reg    <= (others => (others => '0'));
+      i_reg            <= 0;
+      wait_counter_reg <= 0;
+      started_reg      <= false;
     elsif rising_edge(clk) then
-      registers_reg <= registers_next;
+      registers_reg    <= registers_next;
+      i_reg            <= i_next;
+      wait_counter_reg <= wait_counter_next;
+      started_reg      <= started_next;
     end if;
   end process;
 
   --------------------------------------------------------------------
   -- working through the instructions
   --------------------------------------------------------------------
-  process is
+  process (i_reg, registers_reg, started_reg, wait_counter_reg) is
+    variable instruction      : std_logic_vector(INSTR_WIDTH-1 downto 0);
+    variable instruction_name : std_logic_vector(INSTR_NAME_WIDTH-1 downto 0);
+    variable reg              : integer range 0 to REG_NUM-1;
+    variable val              : std_logic_vector(REG_WIDTH-1 downto 0);
+    variable wait_cycles : std_logic_vector(INSTR_WIDTH - INSTR_NAME_WIDTH - 1
+                                            downto 0);
   begin
-    
+    -- default assignments
+    registers_next    <= registers_reg;
+    i_next            <= i_reg;
+    wait_counter_next <= wait_counter_reg;
+    started_next      <= started_reg;
+
+    -- decode instruction
+    instruction      := instructions(i_reg);
+    instruction_name := instruction(INSTR_NAME_WIDTH-1 downto 0);
+
+    -- interpret instruction
+    case instruction_name is
+      ----------------------------------------------------------------
+      when "0000" =>                    -- set reg, val
+        val := instruction(INSTR_WIDTH-1 downto INSTR_NAME_WIDTH + REG_NUM_BITS);
+        reg := to_integer(unsigned(instruction
+                                   (INSTR_NAME_WIDTH + REG_NUM_BITS-1 downto
+                                    INSTR_NAME_WIDTH)));
+        -- set the register
+        registers_next(reg) <= val;
+        i_next              <= i_reg + 1;
+      --------------------------------------------------------------
+      when "0001" =>                    -- wait(cycles)
+        wait_cycles := instruction(INSTR_WIDTH - 1 downto INSTR_NAME_WIDTH);
+        if (not started_reg) then
+          wait_counter_next <= to_integer(unsigned(wait_cycles)) - 1;
+          started_next      <= true;
+        else                            -- count down
+          if wait_counter_reg = 0 then
+            started_next <= false;
+            i_next       <= i_reg + 1;
+          else
+            wait_counter_next <= wait_counter_reg - 1;
+          end if;
+        end if;
+      --------------------------------------------------------------
+      when others => null;
+    end case;
   end process;
+
+
+  --------------------------------------------------------------------
+  -- Input
+  --------------------------------------------------------------------
+  clk   <= iCLK_50;
+  reset <= not iKEY(0);
 
   --------------------------------------------------------------------
   -- Output
--- instructions_lib.vhd
+++ instructions_lib.vhd
@@ -2,7 +2,37 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 ----------------------------------------------------------------------
-package instructions is
+package instructions_lib is
 
-  
-end package instructions;
+  --------------------------------------------------------------------
+  -- Instructions
+  --------------------------------------------------------------------
+  constant INSTR_NAME_WIDTH : integer := 4;
+
+
+  --------------------------------------------------------------------
+  -- 16 32-bit registers
+  --------------------------------------------------------------------
+  constant REG_WIDTH    : integer := 32;
+  constant REG_NUM      : integer := 16;
+  constant REG_NUM_BITS : integer := 4;
+  type reg_type is array (natural range 0 to REG_NUM-1) of
+    std_logic_vector (REG_WIDTH-1 downto 0);
+
+  constant INSTR_WIDTH : integer := INSTR_NAME_WIDTH + REG_WIDTH + REG_NUM_BITS;
+
+  --------------------------------------------------------------------
+  -- User instructions
+  --------------------------------------------------------------------
+  constant INSTRUCTION_COUNT : integer := 4;
+  type instruction_rom_type is array (0 to INSTRUCTION_COUNT - 1) of
+    std_logic_vector (39 downto 0);
+  constant INSTRUCTIONS : instruction_rom_type :=
+    (
+      "0000000000000000000000000000000000000000",
+      "0000000000101111101011110000100000000001",
+      "1111111111111111111111111111111100000000",
+      "0000000000101111101011110000100000000001"
+      );
+
+end package instructions_lib;
