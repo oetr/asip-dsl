b8bd55c65d6f17e815f454b9d7c07945346adacf
Peter Samarin <petersamarin@gmail.com>
2014-12-20 00:52:49 +0100

    Alternative idea---two interpreters

---
+++ asip-interpreter.rkt
@@ -0,0 +1,19 @@
+;; Current problem with the macro solution:
+;; two definitions of the same thing:
+;; 1) simulator,
+;; 2) conversion to vhdl code
+
+;; Possible solution:
+;; an interpreter to convert the DSL code into some form that can
+;; be both: simulated and converted to VHDL based on user choice
+
+;; Another way:
+;; Two interpreters (or one interpreter and one compiler)
+;; for the same code
+;; 1) The first one simulated the code
+;; 2) The second converts it to VHDL
+
+;; The simulator should support concurrent execution
+;; Should probably add delays for all standard operations
+
+
