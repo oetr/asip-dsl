e03791f2aa98cecfe71b6b23d6fe67e87017280f
Peter Samarin <petersamarin@gmail.com>
2014-12-24 02:13:59 +0100

    Slowly increase the amount of recognized expressions
    - also update the DSL syntax as I go

--- asip-interpreter.rkt
+++ asip-interpreter.rkt
@@ -90,7 +90,6 @@
            (check-false (is-procedure-definition?
                          '(def-vector (a) 10))))
 
-
 (define (is-i/o-definition? s-expr)
   (and (list? s-expr)
        (symbol=? 'def-i/o (car s-expr))))
@@ -98,6 +97,15 @@
 (test-case "i/o definition"
            (check-true (is-i/o-definition?
                          '(def-i/o a 10))))
+
+
+(define (is-asip-definition? s-expr)
+  (and (list? s-expr)
+       (symbol=? 'def-asip (car s-expr))))
+
+(test-case "asip definition"
+           (check-false (is-asip-definition?
+                         '(def-asip a 10))))
 
 (define (sim-eval code)
   ;; traverse the code and find/merge:
@@ -119,62 +127,48 @@
          (sim-eval (cdr code))]
         [(is-i/o-definition? expression)
          (printf "i/o definition~n")
-         (sim-eval (cdr code))])
-  ;; extract necessary state from the code
-  ;; registers, initialize them
+         (sim-eval (cdr code))]
+        [(is-asip-definition? expression)
+         (printf "asip definition~n")
+         (sim-eval (cdr code))]
+        [else
+         (error 'sim-eval "unknown expression: ~a~n" expression)])
   )
 
-(sim-eval '((def (a) 10)
-            (def a 10)
-            (def-i/o a 10)))
-
-
-;; to find constant, signals, signal vectors, nested vectors
-(define (find-signal-definitions code)
-  ;; to find signal definitions
-  ;; add to the global signal definitions
-  ;; TODO: consider scope and free variables
-  ()
-  ;; constant is used read-only
-  ;; TODO: signal used as write somewhere in the asip should be recognized
-  ())
-
 ;; Racket style code looks
-(hw
- ;; inputs and outputs
- (def-i/o
-   (iCLK_50 in)
-   (iKEY in 3 0)
-   (oLEDR out 17 0)
-   (oLEDG out 7 0))
+(sim-eval
+ '(
+   ;; inputs and outputs
+   (def-i/o
+     (iCLK_50 in)
+     (iKEY in 3 0)
+     (oLEDR out 17 0)
+     (oLEDG out 7 0))
 
- ;; registers, wires
- (def a (range 10 0) 10)
- (def b (range 10 0) 10)
- (def N 10) ;; constant
- 
- ;; looped signal definition
- ;; will be expanded into 10 signals/registers
- (def-vector c N (range 10 0))
+   ;; registers, wires
+   (def a (range 10 0) 10)
+   (def b (range 10 0) 10)
+   (def N 10) ;; constant
+   
+   ;; looped signal definition
+   ;; will be expanded into 10 signals/registers
+   (def-vector c N (range 10 0))
 
- ;; register logic
- (when (rising-edge iCLK_50)
-   (def temp (ref iKEY 0))
-   (set c 0 (xor c temp)))
+   ;; register logic
+   (when (rising-edge iCLK_50)
+     (def temp (ref iKEY 0))
+     (set c 0 (xor c temp)))
 
- ;; combinatorial logic
- (set b (range 3 0) (ref iKEY (range 3 0)))
- (set b (at 10 9 8 7) (ref iKEY (at 3 2 1 0)))
+   ;; combinatorial logic
+   (set b (range 3 0) (ref iKEY (range 3 0)))
+   (set b (at 10 9 8 7) (ref iKEY (at 3 2 1 0)))
 
- ;; ASIP instructions
- (def-instruction (shift-regs)
-   (set (ref c (range 9 1)) (ref c (range 8 0))))
+   ;; ASIP instructions
+   (def (add-regs)
+        (set c (+ c 10))
+        (set b (+ c 20)))
 
- (def (add-regs)
-      (set c (+ c 10))
-      (set b (+ c 20)))
-
- (define-asip a00
-   (for ([i 0 10]) ;; come out as a counter
-     (add-regs)
-     (add-regs))))
+   (def-asip a00
+     (for ([i 0 10]) ;; come out as a counter
+       (add-regs)
+       (add-regs)))))
